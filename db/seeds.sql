4f416b9fedf8843829a5707d47210220c864f8be
hannerc <31261156+hannerc@users.noreply.github.com>
2018-03-02 18:04:00 +1100

    ugh

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Foot Feta-ish Burger - comes with feta cheese');
+INSERT INTO burgers (burger_name) VALUES ('Mission A-Corn-Plished Burger - comes with corn salsa');
+INSERT INTO burgers (burger_name) VALUES ('Hit Me With Your Best Shallot Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ive Created a Muenster Burger');
